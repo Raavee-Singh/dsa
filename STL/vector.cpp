f1a4ee216154a7b599ad16f9a9592e83482352fa
Raavee Singh <gdg1465@gmail.com>
2026-07-23 09:33:41 +0530

    learing vector and array in stl

---
+++ STL/vector.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<vector>
+int main(){
+    std::vector<int> v;
+    std::vector<int> vec(4,2);
+    std::cout<<"Printing vec: ";
+    for(int i:vec){
+        std::cout<<i<<" ";
+    }
+    std::cout<<std::endl;
+    std::cout<<"Size: "<<v.capacity()<<std::endl;
+    std::cout<<"No. of elements: "<<v.size()<<std::endl;
+    v.push_back(1);
+    std::cout<<"Size: "<<v.capacity()<<std::endl;
+    std::cout<<"No. of elements: "<<v.size()<<std::endl;
+    v.push_back(3);
+    std::cout<<"Size: "<<v.capacity()<<std::endl;
+    std::cout<<"No. of elements: "<<v.size()<<std::endl;
+    v.push_back(3);
+    std::cout<<"Size: "<<v.capacity()<<std::endl;
+    std::cout<<"No. of elements: "<<v.size()<<std::endl;
+
+    std::cout<<"First element: "<<v.front()<<std::endl;
+    std::cout<<"Last element: "<<v.back()<<std::endl;
+
+    std::cout<<"Before pop: ";
+    for(int i:v){
+        std::cout<<i<<" ";  
+    }
+
+    v.pop_back();
+    std::cout<<std::endl;
+    std::cout<<"After pop: ";
+    for(int i:v){
+        std::cout<<i<<" ";
+    }
+    std::cout<<std::endl;
+    std::cout<<"Before clear: "<<v.size()<<std::endl;
+    v.clear();
+    std::cout<<"After clear: "<<v.size()<<std::endl;
+    return 0;
+}
