086f2f2fb5bebf5f48e5c4759b9aaeeb1b907b19
Raavee Singh <gdg1465@gmail.com>
2026-07-25 20:03:13 +0530

    array and vector in stl

--- STL/practice.cpp
+++ STL/practice.cpp
@@ -5,7 +5,7 @@ int main(){
     std::array<int, 5> arr={23,4,2,1,45};
     std::vector<int> vec;
     for(int i:arr){
-        vec.push_back(arr[i]);
+        vec.push_back(i);
     }
     std::cout<<"The vector is: "<<std::endl;
     for(int i:vec){
@@ -13,5 +13,14 @@ int main(){
     }
     std::cout<<std::endl;
     std::cout<<"Printing complete!"<<std::endl;
+
+    std::cout<<"First element of array is: "<<arr.front()<<std::endl;
+    std::cout<<"Last element of array is: "<<arr.back()<<std::endl;
+
+    vec.pop_back();
+    std::cout<<"Printing array after pop: "<<std::endl;
+    for(int i:vec){
+        std::cout<<i<<" ";
+    }
     return 0;
 }
