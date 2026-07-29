7f5dc00e3fc88279db130003ccaf1e21dcf6a29c
Raavee Singh <gdg1465@gmail.com>
2026-07-29 19:23:00 +0530

    practicing deque and list

---
+++ STL/practice2.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<deque>
+#include<list>
+int main(){
+    std::deque<int> deque1;
+    std::list<int> list1;
+    deque1.push_front(1);
+    deque1.push_back(2);
+    deque1.push_back(3);
+    list1.push_front(4);
+    list1.push_back(5);
+    list1.push_back(6);
+    
+}
